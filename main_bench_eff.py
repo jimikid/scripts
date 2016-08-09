d37b36e04cbd92f434f6449feda3eb925bc10d9d
sbaek_lab <sbaek@aelrayyah-ws.enphaseenergy.com>
2016-08-09 16:20:24 -0700

    firmware is set by the script using 'code_update_heron_mode.py'

--- main_bench_eff.py
+++ main_bench_eff.py
@@ -9,10 +9,17 @@ Created on 07/06/2016, @author: sbaek
 
   V02 07/27/2016
   - bootup process is moved to main script.  It needs to be there to change registers.
+
+  V03 08/9/2016
+  - 'code_update_heron_mode' is included in data_aq_lib.  Now plant firmware automatically from a specificed folder para['firmware_path']
+  - if para['firmware_path'] is none, pass
+  - def write_flash is added to change registers
+
 """
 
 import sys, time
 from os.path import abspath, dirname
+import os
 sys.path.append(dirname(dirname(__file__)))
 sys.path.append('%s/data_aq_lib' % (dirname(dirname(__file__))))
 
@@ -21,25 +28,59 @@ from collections import OrderedDict
 from data_aq_lib.equipment import equipment
 import data_aq_lib.equipment.sas as sas
 import data_aq_lib.equipment.ac_source as ac
+import data_aq_lib.equipment.code_update_heron_mod as firm
 
 import data_aq_lib.measurement.measurements as mm
 import data_aq_lib.analysis.figure_functions as ff
 from data_aq_lib.equipment import serialcom
+import data_aq_lib.equipment.dvm as dvm
 
+import data_aq_lib.analysis.eff_func as ef
 
 def initialize():
-    print 'initialize..\n'
+    print ' initialize..\n'
     eq=equipment.Equip()
     list_equip=eq.get_equip()
     return list_equip
+
+def firmware(para, eq):
+    """ To load code into Heron board.
+     - there should not be Vdc when starting.
+     - there should be two files (procload and parameter table)
+     - Board needs to be booted, serial cable attached, uses COM5.
+     - Copy procload & parameter table files to a directory on local drive, and pass the
+    directiry name on command line.
+    """
+    print ' set firmware..'
+    dir = para['firmware_path']
+    files = os.listdir(para['firmware_path'])
+    pl_file, pt_file = None, None
+
+    for name in files:
+        if name.find("parm") != -1:pt_file = "%s/%s" %  (dir, name)
+        if name.find("procload-") != -1: pl_file = "%s/%s" %  (dir, name)
+
+    sas.sas_fixed(eq, CURR=14, VOLT=20)
+    x = firm.PCUProgrammer()
+    x.connect(comport=5)
+    x.enter_flasher_mode()
+    x.erase_procload_and_parm()
+
+    para['log'] += pl_file+'\n'+pt_file+'\n'
+    print ' pl_file : %s' %pl_file
+    print ' pt_file : %s' %pt_file
+
+    for bank in (0,1):
+        x.program_file('pci%i'% bank,pl_file)
+        x.program_file('ppi%i' % bank, pt_file)
+    sas.sas_off(eq)
+    time.sleep(20)
 
 def write_flash(cmd, delay=0):
     '''
        - to change values on registers
        - need to boot up by setting a DC voltage
     :param cmd: a pair with write and read e.g. ('wl f60A 00\r', 'rl f60A 1\r')
-    :param delay:
-    :return:
     '''
     time.sleep(delay)
     try:
@@ -53,33 +94,40 @@ def write_flash(cmd, delay=0):
     except:pass
 
 def save_log(exe, para):
-    #for i in range(exe.__len__()):
-    #    para['log'] += ' '+exe.keys()[i]+': '+str(exe.values()[i])+'\n'
+    para['log'] += '\n'
     for i in range(para.__len__()):
         if para.keys()[i] is not 'log': # to avoid duplication, i is Int
             para['log'] += ' '+para.keys()[i]+': '+str(para.values()[i])+'\n'
-    para['log'] +="\n\n save and close at"+time.strftime(" %m/%d/%Y %I:%M \n")  
+    para['log'] +="\n\n save and close at"+time.strftime(" %m/%d/%Y %I:%M \n")
     text_file = open(para['data_path']+"/log.txt", "w")
     text_file.write(para['log'])
-    text_file.close()    
+    text_file.close()
 
 def measurements(para, eq, delay=4, cmd=None, adj=False):
     data=[]
     fault = True
+
+    ''' wait til reach to temperature set '''
+    if para['set_temp'] is not None:
+        print ' set temperature : %s' %para['set_temp']
+        temp=None
+        while not (temp < (para['set_temp'] +1.0) and temp > (para['set_temp'] -1.0)):
+            time.sleep(30)
+            temp=dvm.measure_tempc(eq)
+
+
     for pts in para['SAS_pts']:
         ''' boot up '''
         if fault :  #only first time and when fault occurs,
             ac.set_ac_source(eq, mode=para['ac_mode'], freq=60.0)
-            sas.sas_pcu_boot(eq, para, CURR=14, VOLT=36, boot_up=1.0)
+            sas.sas_pcu_boot(eq, para, CURR=18, VOLT=30, boot_up=1.0)
             if cmd is not None:
                 for i in cmd:
                     write_flash(cmd=i, delay=1) # need around 20sec to boot up
                     para['log'] += ' %s %s\n'%i # i is always in a pair with write and read
 
-        #'''SAS '''
-        para['SAS_volt']=pts
-        #sas.sas_fixed_adj(eq, CURR=14, VOLT=para['SAS_volt'])
 
+        para['SAS_volt']=pts
         m1=mm.Measurement(para, eq)
         fault= m1.flag
         print m1
@@ -90,91 +138,79 @@ def measurements(para, eq, delay=4, cmd=None, adj=False):
     return results, m1
 
 
-def main(name='name', P_rated=280, mode='LL', Mnt='On', cmd=None, Load_pts=None, SAS_pts=None, adj=False):
-    '''
-    :param name:
-    :param P_rated:
-    :param mode:
-    :param Mnt:
-    :param cmd:
-    :return:
-    '''
+def main(foldername, P_rated, mode, Mnt=True, Firm=False, Plt=True, cmd=None, Load_pts=None, SAS_pts=None, adj=False):
     ''' set parameters'''
     exe, para, eq = OrderedDict(), OrderedDict(), OrderedDict()
-    start= "\n\n start at"+time.strftime(" %m/%d/%Y %I:%M \n\n")
-    print start
-    para['log'] =start
+    results, m1= None, None
+    para['log']= "\n\n start at"+time.strftime(" %m/%d/%Y %I:%M \n\n")
 
-    para['model'], para['SN'], para['pcb']='Hornet_P3', name, '800-00504 05'
-    para['log'] +="\n Description : \n 121629026777   \n check operating range with 91uH (C3 Pri out), parm-NA-hornet1-S290-72-p540-00118-r01-v01.04.92.tch, 422-00158-01 \n\n"
-
-    exe['measurement']=Mnt #'On'        
-    para['data_file_name'] ='data'
-    para['ac_mode'], para['p_rated']=mode, P_rated   #'LN' : (120.0 ,120.0 ,120.0), 'LL':(120.0 120.0)
-    para['folder_name']=para['SN']+'_%s_%sW' %(para['ac_mode'], para['p_rated'])
-    para['data_path']= ff.create_path_dir(name=para['folder_name'], path = dirname(dirname(__file__))+'/data') 
-    para['source_path']=dirname(dirname(__file__))+'/source_files'
-    para['cmd']= cmd
-    file_name='summary_%s_%.0fW' %(para['ac_mode'], para['p_rated'])
-
+    ####################################################################################################################
+    para['Description'] = "\n\n check operating range and eff with Lr 110uH (Pri in) and temperature \n\n"
+    para['ac_mode'], para['p_rated']=mode, P_rated   #'LN' : (120.0 ,120.0 ,120.0), 'LL':(120.0 120.0), 'LL_HVRT' :'LL':(132.0 132.0)
+    para['model'], para['SN'], para['pcb']='Hornet_P3', '121629026786', '800-00504 05'
+    para['firmware_path']='C:/Users/sbaek/WorkSpace/firmware/P3_110uH_wide' #default None
     para['Load_pts'] =  [i*0.01 for i in range(50,70,10)]+[i*0.01 for i in range(70,90,5)]+[i*0.01 for i in range(90,107,2)]
     para['SAS_pts'] =   [i*0.1 for i in range(450,350,-40)]+[i*0.1 for i in range(350,260,-20)]
+    para['set_temp'] = None #default : None, measure temperature before boot up and start at the set_temp
+    ####################################################################################################################
 
+    exe['measurement']=Mnt #'On'
+    para['data_file_name'] ='data'
+    para['folder_name']=foldername+para['SN']+'_%s_%sW' %(para['ac_mode'], para['p_rated'])
+    para['data_path']= ff.create_path_dir(name=para['folder_name'], path = dirname(dirname(__file__))+'/data')
+    para['cmd']= cmd
+    file_name='summary_%s_%.0fW' %(para['ac_mode'], para['p_rated'])
     if Load_pts is not None:
         para['Load_pts'] =Load_pts
     if SAS_pts is not None:
         para['SAS_pts'] = SAS_pts
 
-
-    ''' do measurements'''
     eq=initialize()
-    results, m1=measurements(para, eq, delay=3, cmd=cmd, adj=adj)
-    results=results.set_index([range(len(results))])
+    ''' firmwares'''
+    if (Firm) and (para['firmware_path'] is not None): # to plant firmware, pcu should start without Vdc!
+        firmware(para, eq)
+    else:pass
 
+    ''' measurements '''
+    if Mnt:
+        results, m1=measurements(para, eq, delay=3, cmd=cmd, adj=adj)
+        results=results.set_index([range(len(results))])
+        print '\n save data to %s' %(para['data_path']+'/'+file_name+'.csv')
+        results.to_csv(para['data_path']+'/'+file_name+'.csv')
+        save_log(exe, para)
+    else: pass
 
-    print '\n save data to %s' %(para['data_path']+'/'+file_name+'.csv')
-    results.to_csv(para['data_path']+'/'+file_name+'.csv')
-    save_log(exe, para)    
-    
-    try:
-        ff.plot_eff(para, file_name)        
-    except:pass
-    return eq, para, m1
+    ''' plots '''
+    if Plt:
+        try:
+            ef1=ef.eff(file_name=file_name, para=para)
+            ef1.plot_map()
+            ef1.plot_eff(xlim=(26, 46), ylim=(95.0, 97.5))
+        except:pass
+    else: pass
+
+    return m1
 
 
 if __name__ == '__main__':
+    temp='25'
 
-    for i in range(1, 2):
-        main(name='C4_eff_%s' %i, P_rated=300, Load_pts=[0.5, 0.75, 1.0], SAS_pts=[i*0.1 for i in range(450,260,-30)], adj=True)
+    mode='LL'
+    for i in range(1, 4):
+        m1=main(foldername='%sC_%s_' %(temp,i), P_rated=300, mode=mode, Firm=True)
+    for i in range(1, 4):
+        m1=main(foldername='%sC_%s_' %(temp,i), P_rated=290, mode=mode, Load_pts=[0.5, 0.75, 1.0], SAS_pts=[27.0, 36.0, 45.0], adj=True)
 
-    for i in range(1, 2):
-        main(name='C4_eff_%s' %i, P_rated=290, Load_pts=[0.5, 0.75, 1.0], SAS_pts=[i*0.1 for i in range(450,260,-30)], adj=True)
+    mode='LL_HVRT'
+    for i in range(1, 4):
+        m1=main(foldername='%sC_%s_' %(temp,i), P_rated=300, mode=mode)
+    for i in range(1, 4):
+        m1=main(foldername='%sC_%s_' %(temp,i), P_rated=290, mode=mode, Load_pts=[0.5, 0.75, 1.0], SAS_pts=[27.0, 36.0, 45.0], adj=True)
 
-    for i in range(1, 2):
-        main(name='C4_eff_%s' %i, P_rated=280, Load_pts=[0.5, 0.75, 1.0], SAS_pts=[i*0.1 for i in range(450,260,-30)], adj=True)
+    mode='LN'
+    for i in range(1, 4):
+        m1=main(foldername='%sC_%s_' %(temp,i), Mnt=False, P_rated=300, mode=mode)
+    for i in range(1, 4):
+        m1=main(foldername='%sC_%s_' %(temp,i), Mnt=False, P_rated=290, mode=mode, Load_pts=[0.5, 0.75, 1.0], SAS_pts=[27.0, 36.0, 45.0], adj=True)
 
-    for i in range(1, 2):
-        main(name='C4_eff_%s' %i, P_rated=270, Load_pts=[0.5, 0.75, 1.0], SAS_pts=[i*0.1 for i in range(450,260,-30)], adj=True)
-
-
-    '''
-    cmd=(('wl F60A 00\r', 'rl F60A 1\r'), ('wl F612 11\r', 'rl F612 1\r'))  #F has to be capital F, string read from serial starts with a capital letter.
-    for i in range(1, 3):
-        main(name='f60A00_f61211_%s' %i, P_rated=290, cmd=cmd, Load_pts=[0.5, 0.75, 1.0], SAS_pts=[27.0, 36.0, 45.0], adj=True)
-    for i in range(1, 3):
-        eq, para, m1=main(name='f60A00_f61211_%s' %i, P_rated=300, cmd=cmd)
-    for i in range(3, 5):
-        main(name='f60A00_f61211_%s' %i, P_rated=290, cmd=cmd, Load_pts=[0.5, 0.75, 1.0], SAS_pts=[27.0, 36.0, 45.0], adj=True)
-
-
-    cmd=(('wl F60A 01\r', 'rl F60A 1\r'), ('wl F612 10\r', 'rl F612 1\r'))
-    for i in range(1, 3):
-        main(name='f60A01_f61210_%s' %i, P_rated=290, cmd=cmd, Load_pts=[0.5, 0.75, 1.0], SAS_pts=[27.0, 36.0, 45.0], adj=True)
-    for i in range(1, 3):
-        eq, para, m1=main(name='f60A01_f61210_%s' %i, P_rated=300, cmd=cmd)
-    for i in range(3, 5):
-        main(name='f60A01_f61210_%s' %i, P_rated=290, cmd=cmd, Load_pts=[0.5, 0.75, 1.0], SAS_pts=[27.0, 36.0, 45.0], adj=True)
-    '''
     m1.shutdown()
-
-  
